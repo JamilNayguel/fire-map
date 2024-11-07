4a6f0c1aa536e49b6bf8591d6717d9f6e5ba7d72
JamilNayguel <jamilcastro2001@gmail.com>
2024-11-07 17:16:58 -0400

    create file enviroment and interface

---
+++ frontend/src/app/environment/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+
+    base_url: 'http://192.168.116.88:3000/api/v1/',
+    base_url_public: 'http://192.168.117.89:3000/api/v1/modis_nrt',
+
+
+  };
